fd836cae0985c37e3c230b5c7b2d006ea869ca65
zxfbj <zxf8517@icloud.com>
2026-01-07 11:19:11 +0800

    增加备注文件

--- docker的一些常用命令.sh
+++ docker的一些常用命令.sh
@@ -1,3 +1,4 @@
-docker exec -it kylinV10 /bin/bash
+docker exec -it kylinV10 /bin/bash # 进入kylin容器内部，执行bash命令
 
-docker run -d --name kylinV10 kylinv10/kylin:b09 tail -f /dev/null
+docker run -d --name kylinV10 kylinv10/kylin:b09 tail -f /dev/null #创建并启动kylinV10容器，并让容器持续运行
+
